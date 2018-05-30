09d4be0451878259fe654c2632438499331d1827
syedasadhasan <Syed.Asad@BCSDOMAIN.local>
2018-05-30 20:39:10 +0530

    remove slack webhook uri

--- post-to-slack.js
+++ post-to-slack.js
@@ -1,23 +1,10 @@
 
 
 exports.postToSlack = function (msg) {
- //   console.log(msg);
-//     const { IncomingWebhook } = require('@slack/client');
-//     const url = 'https://hooks.slack.com/services/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';
-//     const webhook = new IncomingWebhook(url);
-// // Send simple text to the webhook channel
-// webhook.send(msg, function(err, res) {
-//     if (err) {
-//         console.log('Error:', err);
-//     } else {
-//         console.log('Message sent: ', res);
-//     }
-// });
-
 
 var Slack = require('slack-node');
 
-webhookUri = "https://hooks.slack.com/services/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";
+webhookUri = "";
 
 slack = new Slack();
 slack.setWebhook(webhookUri);
